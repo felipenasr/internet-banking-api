faf7b9a74f6c8e7f3723ad54abab80f9d8b54559
felipenasr <felipenricci@gmail.com>
2018-01-30 20:53:47 -0200

    alteracoes no acc_info

--- infra/connectionFactory.js
+++ infra/connectionFactory.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://grptres:grupo3gama@ds119078.mlab.com:19078/internet_banking';
+const url = 'mongodb://grptres:avanade2018@ds119078.mlab.com:19078/internet_banking';
 const database = 'internet_banking';
 
 
